92d9cfb14b79a24e555b52923e83972ec0d39abf
Tolulope Egbetokun <tegbetokun@gmail.com>
2021-06-26 04:19:39 -0400

    First commit

---
+++ Lab4/Models/Community.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Community
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Registration Number")]
+        [Required]
+        public String Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        public ICollection<CommunityMembership> CommunityMemberships { get; set; }
+
+    }
+}
---
+++ Lab4/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        public String FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        public ICollection<CommunityMembership> CommunityMemberships { get; set; }
+    }
+}
